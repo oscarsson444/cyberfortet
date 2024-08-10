a63edf8aad902d1004ce9a92a3610ac745f79caf
oscarsson444 <oscarsson444@gmail.com>
2024-08-10 13:56:52 +0200

    added game to home menu.

--- src/components/HomeMenu.jsx
+++ src/components/HomeMenu.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export default function HomeMenu() {
   return (
     <div className="flex justify-center items-center">
-      <div className="grid grid-cols-2 gap-10">
+      <div className="grid grid-cols-3 gap-10">
         <div className="home-menu-item">
           <Link to="/articles">
             <GrArticle className="w-20 h-20 mx-auto" />
@@ -18,6 +18,12 @@ export default function HomeMenu() {
           <Link to="/about">
             <BsPerson className="w-20 h-20 mx-auto" />
             <p>Om mig</p>
+          </Link>
+        </div>
+        <div className="home-menu-item">
+          <Link to="/rocket">
+            <BsPerson className="w-20 h-20 mx-auto" />
+            <p>Game (WIP)</p>
           </Link>
         </div>
       </div>
