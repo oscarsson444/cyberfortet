5e9134c263e045e1e1ec88cadd48fcab7de53495
oscarsson444 <oscarsson444@gmail.com>
2024-08-11 15:49:21 +0200

    added more features to the game.

--- src/game/main.js
+++ src/game/main.js
@@ -1,31 +1,30 @@
-import { Boot } from './scenes/Boot';
-import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import Phaser from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import { Boot } from "./scenes/Boot";
+import { Game } from "./scenes/Game";
+import { GameOver } from "./scenes/GameOver";
+import { MainMenu } from "./scenes/MainMenu";
+import Phaser from "phaser";
+import { Preloader } from "./scenes/Preloader";
 
 // Find out more information about the Game Config at:
 // https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        Game,
-        GameOver
-    ]
+  type: Phaser.AUTO,
+  width: 800,
+  height: 800,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  parent: "game-container",
+  backgroundColor: "#ffffff",
+  scene: [Boot, Preloader, MainMenu, Game, GameOver],
 };
 
 const StartGame = (parent) => {
-
-    return new Phaser.Game({ ...config, parent });
-
-}
+  return new Phaser.Game({ ...config, parent });
+};
 
 export default StartGame;
--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -1,30 +1,62 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
-    constructor ()
-    {
-        super('Game');
+export class Game extends Scene {
+  landingPads;
+  player;
+  cursors;
+
+  constructor() {
+    super("Game");
+  }
+
+  create() {
+    this.add.image(400, 400, "background").setScale(2);
+
+    this.player = this.physics.add.sprite(400, 600, "ship");
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+    this.player.setAngularDrag(300);
+
+    this.landingPads = this.physics.add.staticGroup();
+
+    let groundRect = this.add.rectangle(400, 800, 800, 20, 0xffffff); // Adjust width, height, and color as needed
+    this.physics.add.existing(groundRect, true);
+    this.landingPads.add(groundRect);
+
+    let landingPadRect1 = this.add.rectangle(400, 400, 100, 20, 0xffffff); // Adjust width, height, and color as needed
+    this.physics.add.existing(landingPadRect1, true);
+    this.landingPads.add(landingPadRect1);
+
+    this.physics.add.collider(this.player, this.landingPads);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.up.isDown) {
+      let angle = this.player.angle * (Math.PI / 180);
+      let x = Math.cos(angle + Math.PI / 2);
+      let y = Math.sin(angle + Math.PI / 2);
+      this.player.setAcceleration(-x * 200, -y * 400);
+    } else {
+      this.player.setAcceleration(0, 0);
+    }
+    if (this.cursors.left.isDown) {
+      this.player.setAngularAcceleration(-300);
+    } else if (this.cursors.right.isDown) {
+      this.player.setAngularAcceleration(300);
+    } else {
+      this.player.setAngularAcceleration(0);
     }
 
-    create ()
-    {
-        this.cameras.main.setBackgroundColor(0x00ff00);
-
-        this.add.image(512, 384, 'background').setAlpha(0.5);
-
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-
-        EventBus.emit('current-scene-ready', this);
+    // Friction with the ground
+    if (this.player.body.touching.down || this.player.body.onWorldBounds) {
+      this.player.setVelocity(this.player.body.velocity.x * 0.9, 0);
     }
+  }
 
-    changeScene ()
-    {
-        this.scene.start('GameOver');
-    }
+  changeScene() {
+    this.scene.start("GameOver");
+  }
 }
